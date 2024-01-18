d3083759d33b3d6d8a96fe65ed53bf2dd711c986
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-01-18 11:18:12 +0700

    fix: change background header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
-    <header className="fixed left-0 z-50 flex h-[60px] w-full cursor-pointer items-center px-60 ">
+    <header className="fixed left-0 z-50 flex h-[60px] w-full cursor-pointer items-center bg-white px-60">
       <h1 className="mr-32 text-2xl font-bold ">YoungCodersGenz</h1>
       <ul className="flex items-center gap-10 text-xs *:cursor-pointer *:font-bold *:tracking-widest *:text-text_defautl 2xl:*:text-base ">
         <li className="menu-items-hover hover:text-primary">Trang chủ</li>
