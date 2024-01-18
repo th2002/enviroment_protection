b3a11d3578ceafedacba94dfee0169581d1a1a1b
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-01-18 11:15:23 +0700

    fix: banner layout responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="fixed left-0 z-50 flex h-[60px] w-full cursor-pointer items-center px-60 ">
       <h1 className="mr-32 text-2xl font-bold ">YoungCodersGenz</h1>
-      <ul className="flex items-center gap-10 *:cursor-pointer *:font-bold *:tracking-widest *:text-text_defautl ">
+      <ul className="flex items-center gap-10 text-xs *:cursor-pointer *:font-bold *:tracking-widest *:text-text_defautl 2xl:*:text-base ">
         <li className="menu-items-hover hover:text-primary">Trang chủ</li>
         <li className="menu-items-hover hover:text-primary">
           Sự kiện <FontAwesomeIcon icon={faStar} fade color="#cf2e2e" />
