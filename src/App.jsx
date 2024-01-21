963dcc1e861ecb44ace79658cd89f2949736a07d
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-01-22 00:26:25 +0700

    feat: create content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import Banner from "~/components/banner/Banner";
 import Header from "~/components/header/Header";
 import Sidebar from "~/components/sidebar/Sidebar";
-
+import Content from "./components/content/Content";
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <>
+      <Helmet>
+        <script
+          async
+          defer
+          crossOrigin="anonymous"
+          src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v18.0"
+          nonce="fyHes9kk"
+        />
+      </Helmet>
+      <div id="fb-root"></div>
       <div className="flex min-h-screen flex-col justify-start overflow-hidden px-20 font-roboto">
         <Header />
         <Banner />
         <div className="mb-8 mt-8 flex">
           <Sidebar />
+          <Content />
         </div>
       </div>
     </>
