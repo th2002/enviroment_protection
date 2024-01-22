ab13c34caea29978cb5db8a5470a31d8d0b75aa7
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-01-23 00:42:13 +0700

    fix(App.jsx): remove script gg Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,6 @@ function App() {
   return (
     <>
       <Helmet>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-7N8DQ7659Q"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-7N8DQ7659Q');
-        </script>
-      </Helmet>
-      <Helmet>
         <script
           async
           defer
