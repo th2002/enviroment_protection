f2a0d301596cab168a60d938ca001891de310a29
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-01-22 21:11:18 +0700

    feat: add animation for sidebar left

--- src/Reveal.jsx
+++ src/Reveal.jsx
@@ -1,21 +1,53 @@
 /* eslint-disable react/prop-types */
 
-import { motion } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 function Reveal({ children, width = "fit-content" }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
   return (
-    <div style={{ position: "relative", width, overflow: "hidden" }}>
+    <div ref={ref} style={{ position: "relative", width, overflow: "hidden" }}>
       <motion.div
         variants={{
           hidden: { opacity: 0, y: 75 },
           visible: { opacity: 1, y: 0 },
         }}
         initial="hidden"
-        animate="visible"
+        animate={mainControls}
         transition={{ duration: 0.5, delay: 0.25 }}
       >
         {children}
       </motion.div>
+
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        style={{
+          position: "absolute",
+          top: 4,
+          bottom: 4,
+          left: 0,
+          right: 0,
+          backgroundColor: "#00b894",
+          zIndex: 20,
+        }}
+      ></motion.div>
     </div>
   );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,59 +1,59 @@
 function Sidebar() {
   return (
-    <div className="flex flex-col gap-6 border-r-[1px] border-gray-300 pr-20">
+    <div className="flex h-[680px] flex-col gap-6 overflow-auto scroll-smooth border-r-[1px] border-gray-300 pr-20 2xl:h-[754px] 3xl:h-auto">
       <h2 className="font-semibold tracking-widest">Cuộc thi</h2>
       <ul className="*:border-l-2 *:border-gray-400 *:py-3 *:pl-5 *:text-sm *:font-medium *:text-gray-500 *:transition-all *:duration-300 *:ease-linear">
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Lý do</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#why">Lý do</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Hình thức tham gia</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#participate">Hình thức tham gia</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Quy tắc</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#rule">Quy tắc</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Lịch trình</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#schedule">Lịch trình</a>
         </li>
       </ul>
 
       <h2 className="font-semibold tracking-widest">Giám khảo</h2>
       <ul className="*:border-l-2 *:border-gray-400 *:py-3 *:pl-5 *:text-sm *:font-medium *:text-gray-500 *:transition-all *:duration-300 *:ease-linear">
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Danh sách giám khảo</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#examiner">Danh sách giám khảo</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Tiêu chí đánh giá</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#criteria">Tiêu chí đánh giá</a>
         </li>
       </ul>
 
       <h2 className="font-semibold tracking-widest">Giải thưởng</h2>
       <ul className="*:border-l-2 *:border-gray-400 *:py-3 *:pl-5 *:text-sm *:font-medium *:text-gray-500 *:transition-all *:duration-300 *:ease-linear">
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Danh sách giải thưởng</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#prize">Danh sách giải thưởng</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Quy trình trao giải</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#procedure">Quy trình trao giải</a>
         </li>
       </ul>
 
       <h2 className="font-semibold tracking-widest">Bảo vệ môi trường</h2>
       <ul className="*:border-l-2 *:border-gray-400 *:py-3 *:pl-5 *:text-sm *:font-medium *:text-gray-500 *:transition-all *:duration-300 *:ease-linear">
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Bài Viết</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#posts">Bài Viết</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Thông Tin</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#info">Thông Tin</a>
         </li>
       </ul>
 
       <h2 className="font-semibold tracking-widest">Liên hệ</h2>
       <ul className="*:border-l-2 *:border-gray-400 *:py-3 *:pl-5 *:text-sm *:font-medium *:text-gray-500 *:transition-all *:duration-300 *:ease-linear">
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Fanpage</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#fanpage">Fanpage</a>
         </li>
-        <li className="hover:border-gray-900 hover:text-gray-900">
-          <a href="#">Email</a>
+        <li className="hover:border-primary hover:text-primary">
+          <a href="#contact">Email</a>
         </li>
       </ul>
     </div>
